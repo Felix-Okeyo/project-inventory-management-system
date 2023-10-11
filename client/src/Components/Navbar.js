24b56c82b476a6218277d4a20e1d9f909e9e45df
JOY19ANYANGO <joyanyango402@gmail.com>
2023-10-12 00:29:33 +0300

    done frontend login and signup

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,33 +1,21 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import "./Form.css";
+// Import your LoginForm and SignUpForm components
+
 
 function NavBar() {
-  const [isLoginFormOpen, setLoginFormOpen] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  
 
-  const toggleLoginForm = () => {
-    setLoginFormOpen((prev) => !prev);
-  };
-
-  const handleLogin = () => {
-    // Implement your login logic here
-    console.log("Username:", username);
-    console.log("Password:", password);
-    // Reset form after login
-    setUsername("");
-    setPassword("");
-    // Close the login form after successful login (You can handle actual authentication here)
-    setLoginFormOpen(false);
-  };
+ 
 
   return (
     <header className="navbar">
       <div className="List">
         <ul className="list">
           <li>
-            <Link to="/" className="nav-links">
+            <Link to="/home" className="nav-links">
               Home
             </Link>
           </li>
@@ -50,45 +38,9 @@ function NavBar() {
         </ul>
       </div>
 
-      <button
-        type="button"
-        className="button"
-        onClick={toggleLoginForm}
-        style={{
-          color: "white",
-          marginTop: "-65px",
-          float: "right",
-          marginRight: "65px",
-          borderRadius: "100px",
-        }}
-      >
-        {isLoginFormOpen ? "Close" : "Sign in"}
-      </button>
-
-      {isLoginFormOpen && (
-        <div className="login-form-container">
-          <form className="login-form">
-            <h2>Login Form</h2>
-            <input
-             className="input-field"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-             className="input-field"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin} className="button">
-              Login
-            </button>
-          </form>
-        </div>
-      )}
+      
+     
+      
     </header>
   );
 }
